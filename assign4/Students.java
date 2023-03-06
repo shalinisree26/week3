c80313ce5c0556221312ab9aee78dc984f1994f1
shalinisree26 <126102119+shalinisree26@users.noreply.github.com>
2023-03-06 20:04:57 +0530

    Add files via upload

--- assign4/Students.java
+++ assign4/Students.java
@@ -3,27 +3,27 @@ package assign4;
 public class Students {
 	
 	public void getStudentInfo(int idnumber) {
-System.out.println("student id number");	
+System.out.println("student idcard number: " +idnumber);	
 }
 	
-	public void getStudentInfo(int id,String name) {
-		System.out.println("student id and name :");
+	public void getStudentInfo(int id, String name) {
+		System.out.println("student id and name :" +id +" " +name  );
 	}
 	public void getStudentInfo(String email) {
-		System.out.println("student email is :");
+		System.out.println("student email is :" + email);
 	}
-	public void getStudentInfo(long mobilenumber) {
-		System.out.println("mobile number is :");
+	public void getStudentInfo(double attendance) {
+		System.out.println("attendance :" + attendance);
 	}
 
 
 	public static void main(String[] args) {
 		
 		Students work=new Students();
-		work.getStudentInfo("123");
-		work.getStudentInfo("123, shalini");
-		work.getStudentInfo("abc@xygmail.com");
-		work.getStudentInfo("9999999999");		
+		work.getStudentInfo(123);
+		work.getStudentInfo(123, "shalini");
+		work.getStudentInfo("abcxy@gmail.com");
+		work.getStudentInfo(85.9);		
 		
 		
 	}
